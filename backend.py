7729ae8bc6274b9cc581a4042ef00f3c6ddc582b
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-03-07 19:47:23 +0000

    add error handling ,add sky condition

--- backend.py
+++ backend.py
@@ -3,17 +3,13 @@ import env
 import requests
 
 
-def get_data(place, forecast_days, kind=None):
+def get_data(place, forecast_days):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={os.getenv('API_KEY')}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data['list']
     nr_values = 8*forecast_days
     filtered_data = filtered_data[:nr_values]
-    if kind == 'Temperature':
-        filtered_data = [dict['main']['temp'] for dict in filtered_data]
-    elif kind == 'Sky':
-        filtered_data = [dict['weather'][0]['main'] for dict in filtered_data]
     return filtered_data
 
 
--- main.py
+++ main.py
@@ -5,17 +5,32 @@ import os
 
 st.title('Weather Forecast for the next days')
 
-cities_box_choice = st.selectbox('Place:', ['Dublin', 'Limerick'])
+cities_box_choice = st.text_input('Place:')
 forecasted_days = st.slider(
     'Forecast Days', 1, 5, value=3, help='Select number of forecasted days')
 data_selection = st.selectbox('Select data to view', ['Temperature', 'Sky'])
-st.subheader(
-    f'{data_selection} for the next {forecasted_days} in {cities_box_choice}')
 
 
-days_quantity, temp = backend.get_data(
-    cities_box_choice, forecasted_days, data_selection)
-figure = px.line(x=days_quantity, y=temp, labels={
-                 "x": "Date", "y": "Temperatures"})
+if cities_box_choice:
+    try:
+        filtered_data = backend.get_data(
+            cities_box_choice, forecasted_days)
+        st.subheader(
+            f'{data_selection} for the next {forecasted_days} in {cities_box_choice}')
+        if data_selection == 'Temperature':
+            temperatures = [dict['main']['temp'] -
+                            272.15 for dict in filtered_data]
+            dates = [dict['dt_txt'] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={
+                "x": "Date", "y": "Temperatures"})
+            st.plotly_chart(figure)
 
-st.plotly_chart(figure)
+        elif data_selection == 'Sky':
+            images = {'Clear': 'images/clear.png', 'Clouds': 'images/cloud.png',
+                      'Rain': 'images/rain.png', 'Snow': 'images/snow.png'}
+            sky_conditions = [dict['weather'][0]['main']
+                              for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            st.image(image_paths, width=115)
+    except KeyError:
+        st.error('City provided doesn\'t exist', icon="🚨")
